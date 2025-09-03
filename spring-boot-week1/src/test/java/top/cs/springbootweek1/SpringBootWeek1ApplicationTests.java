b60937723140b858529fa645545efd0d0c1cff06
we180 <70900964+we180@users.noreply.github.com>
2025-09-03 11:13:40 +0800

    init: 初始化 spring-boot-course 项目

---
+++ spring-boot-week1/src/test/java/top/cs/springbootweek1/SpringBootWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.cs.springbootweek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeek1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
