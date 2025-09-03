5e99360013f16ee267e745c79a1b21785a0d0234
we180 <70900964+we180@users.noreply.github.com>
2025-09-03 13:09:56 +0800

    init: 初始化 spring-boot-course 项目

--- spring-boot-week1/src/main/java/top/cs/springbootweek1/SpringBootWeek1Application.java
+++ spring-boot-week1/src/main/java/top/cs/springbootweek1/SpringBootWeek1Application.java
@@ -2,12 +2,19 @@ package top.cs.springbootweek1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootWeek1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWeek1Application.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world666";
+    }
 }
